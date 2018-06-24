1f7b3583accd9cef5ab73762428d40021afee3bd
unknown <boris.radojicic63@gmail.com>
2018-06-24 17:36:05 +0200

    add todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-let {mongoose} = require('./db/mongoose'); 
-let {Todo} = require('./models/todo');
-let {user} = require('./models/user');
+let { mongoose } = require('./db/mongoose');
+let { Todo } = require('./models/todo');
+let { user } = require('./models/user');
 
-let app = express(); 
+let app = express();
 
 app.use(bodyParser.json());
 
@@ -23,10 +24,30 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos})
+        res.send({ todos })
     }).catch((e) => {
         res.status(400).send(res);
     });
+});
+
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+        return console.log('id not valid');
+    }
+    // if(!mongoose.Types.ObjectID.isValid('1234')) {
+    //     return console.log('Invalid id');
+    // }
+    Todo.findById(id).then((todos) => {
+        if (!todos) {
+            res.send('No todos with provided id found');
+            return console.log('no documents with provided id');
+        }
+        res.send(JSON.stringify({todos}, undefined, 5));
+        console.log(JSON.stringify(todos, undefined, 5));
+    }).catch((e) => res.status(400).send());
 });
 
 // GET /todos/dhjdagjagjg
@@ -35,6 +56,6 @@ app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
 
 
