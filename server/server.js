0e0e7d8b670a86c3766729e086385b3393718db7
unknown <boris.radojicic63@gmail.com>
2018-06-24 11:21:49 +0200

    add get todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }).catch((e) => {
+        res.status(400).send(res);
+    })
+});
+
 // GET /todos/dhjdagjagjg
 
 app.listen(3000, () => {
